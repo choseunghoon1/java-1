c277b899e392329867cdbc2d2fb0a8314ab4de59
choseunghoon1 <choseunghoon@kbu.ac.kr>
2026-09-11 13:55:11 +0900

    Practice Java week02

--- week02/sample02-04/src/Main.java
+++ week02/sample02-04/src/Main.java
@@ -1,8 +1,14 @@
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
-void main() {신규 *
-        Scanner keyboard = new Scanner(System.in);
-        String
+void main() {
+    Scanner keyboard = new Scanner(System.in);
+    String
 
+
+
+
+
+    name = keyboard.nextLine();
+    System.out.printf("%s님의 나이는 ?", name);
 
 }
