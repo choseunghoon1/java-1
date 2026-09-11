4c16e32911d2489c21e8c8d5a259952e7261feab
choseunghoon1 <choseunghoon@kbu.ac.kr>
2026-09-11 13:59:21 +0900

    2주차 수업내용

--- week02/sample02-04/src/Main.java
+++ week02/sample02-04/src/Main.java
@@ -8,13 +8,16 @@ void main() {
     float weight;
 
     System.out.print("당신의 이름은 ? ");
-    name
-
-
-
-
-
     name = keyboard.nextLine();
-    System.out.printf("%s님의 나이는 ?", name);
+    System.out.printf("%s의 나이는 ?", name);
+    age = keyboard.nextInt();
+    System.out.printf("%s님의 키는(예 170.5) ?", name);
+    height = keyboard.nextDouble();
+    System.out.printf("%s님의 몸무게는(예 78.6) ?", name);
+    weight = keyboard.nextFloat();
 
+
+    System.out.printf("%s님의 나이는 %d살 입니다.\n", name, age);
+    System.out.printf("%s님의 키는 %.f Cm 입니다.\n", name, height);
+    System.out.printf("%s님의 몸무게는 %.1f Kg 입니다.\n", name, weight);
 }
