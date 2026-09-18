9d7fa261e9b3fdb0eea862f98a334225e9c46f60
choseunghoon1 <choseunghoon@kbu.ac.kr>
2026-09-18 13:13:57 +0900

    3주차 수업내용

--- week 03/sample03-01/src/Main.java
+++ week 03/sample03-01/src/Main.java
@@ -3,7 +3,11 @@
 void main() {
     boolean flag;   //false;
     byte value1 = -128;   //  -128 ~ 127
-    short value2 = 32768;   // - 32,768 ~ + 32767;
+    short value2 = 32767;   // - 32,768 ~ + 32767;
     int value3 = 444444444;
     long value4 = 444444444444L;
+    double value5 = 4.5555555555555555555555555;
+    float value6 = 4.55555555555555555555555555f;
+    char value7 = 'A';
+    String value8 = "AB";
 }
